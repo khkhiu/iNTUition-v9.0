dacc21fbbfdd5121418d1713a2a25a7af9bd2512
khkhiu <khkhiu@gmail.com>
2023-02-25 15:05:34 +0800

    Create tetris.js

---
+++ tetris.js
@@ -0,0 +1,22 @@
+class Tetris {
+    constructor(imageX, imageY, template){
+        this.imageX=imageX;
+        this.imageY=imageY;
+        this.template=template;
+    }
+
+    checkBottom(){}
+
+    checkLeft(){}
+
+    checkRight(){}
+
+    moveBottom(){}
+
+    moveLeft(){}
+
+    moveRight(){}
+
+    changeRotation() {}
+}
+
